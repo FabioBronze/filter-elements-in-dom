d0a72779fa75fbc14d2c65d7ba1d0c76877121da
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-30 22:29:32 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 "use strict";
-const filterElement = document.querySelector("#filter");
+const filterElements = document.querySelector("#filter");
 const cards = document.querySelectorAll(".cards li");
 
-filterElement.addEventListener("input", () => {
-  if (filterElement.value !== "") {
+filterElements.addEventListener("input", () => {
+  if (filterElements.value !== "") {
     for (let card of cards) {
       let title = card.querySelector("h2");
       title = title.textContent.toLowerCase();
-      let filterText = filterElement.value.toLowerCase();
+      let filterText = filterElements.value.toLowerCase();
       if (!title.includes(filterText)) {
         card.style.display = "none";
       } else {
